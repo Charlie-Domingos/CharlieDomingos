6242e160de082976db3e6c243aa1665579d7611a
CHARLIES AUGUSTO DOMINGOS DE OLIVEIRA <PS027036@corp.assai.com.br>
2023-08-18 11:20:23 -0300

    layout botao menu hamb, tema dark

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // Mobile Toggle
 
-const setmobile = document.getElementById("set-mobile");
+const setmobile = document.getElementById('set-mobile')
 
 function toggleMenu(event) {
-  if (event.type === "touchstart") event.preventDefault();
-  const nav = document.getElementById("nav");
-  nav.classList.toggle("active");
+  if (event.type === 'touchstart') event.preventDefault()
+  const nav = document.getElementById('nav')
+  nav.classList.toggle('active')
 }
+
+setmobile.addEventListener('click', toggleMenu)
+setmobile.addEventListener('touchstart', toggleMenu)
+
+// recolhe menu ao clicar
 
 setmobile.addEventListener("click", toggleMenu);
 setmobile.addEventListener("touchstart", toggleMenu);
@@ -19,46 +24,15 @@ menuOptions.forEach((option) => {
   });
 });
 
-function scrollToContato() {
-  const nav = document.getElementById("nav"); // Obtém o elemento do menu
-  nav.classList.remove("active"); // Remove a classe 'active' do menu
-
-  const contatoSection = document.getElementById("contact"); // Obtém a seção de contato
-  contatoSection.scrollIntoView({ behavior: "smooth" }); // Rola até a seção de contato
-}
-
-// Dark mode
-
-const themeToggle = document.getElementById("themeToggle");
-const body = document.body;
-
-themeToggle.addEventListener("click", () => {
-  body.classList.toggle("dark");
-  // Salvar a preferência do usuário em localStorage ou cookies
-  if (body.classList.contains("dark")) {
-    localStorage.setItem("theme", "dark");
-  } else {
-    localStorage.setItem("theme", "light");
-  }
-});
-
-// Verificar a preferência do usuário ao carregar a página
-document.addEventListener("DOMContentLoaded", () => {
-  const savedTheme = localStorage.getItem("theme");
-  if (savedTheme === "dark") {
-    body.classList.add("dark");
-  }
-});
-
-const element = document.querySelector(".container-text");
 let scrolled = false;
 
 function handleScroll() {
-  if (!scrolled && window.scrollY > 100) {
+  if (!scrolled && window.scrollY > 500) {
     container - text.classList.add("active");
     scrolled = true;
   }
 }
+
 
 window.addEventListener("scroll", handleScroll);
 
@@ -85,17 +59,48 @@ function handleAnchorClick(event) {
   }
 }
 
-// Adiciona o ouvinte de evento de clique a todos os links de âncora
-anchorLinks.forEach((link) => {
-  link.addEventListener("click", handleAnchorClick);
+anchorLinks.forEach(link => {
+  link.addEventListener('click', handleAnchorClick);
 });
+
+//scroll botao contato
+function scrollToContato() {
+  const nav = document.getElementById("nav");
+  nav.classList.remove("active"); // Remove a classe 'active' do menu
+
+  const contatoSection = document.getElementById("contact"); // Obtém a seção de contato
+  contatoSection.scrollIntoView({ behavior: "smooth" }); // Rola até a seção de contato
+}
+
+// Dark mode
+
+const themeToggle = document.getElementById('themeToggle')
+const body = document.body
+
+themeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark')
+  // Salvar a preferência do usuário em localStorage ou cookies
+  if (body.classList.contains('dark')) {
+    localStorage.setItem('theme', 'dark')
+  } else {
+    localStorage.setItem('theme', 'light')
+  }
+})
+
+// Verificar a preferência do usuário ao carregar a página
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('theme')
+  if (savedTheme === 'dark') {
+    body.classList.add('dark')
+  }
+})
 
 // Mostrar conteudo gradativamente
 
 window.sr = ScrollReveal({ reset: true });
 
-ScrollReveal().reveal(".projects", {
-  delay: 800,
+ScrollReveal().reveal(".primary-write", {
+  delay: 300,
   rotate: {
     x: 80,
     Y: 0,
@@ -103,38 +108,14 @@ ScrollReveal().reveal(".projects", {
   },
 });
 
-ScrollReveal().reveal(".container-text", {
-  delay: 800,
-  rotate: {
-    x: 80,
-    Y: 0,
-    z: 0,
-  },
-});
-
-ScrollReveal().reveal(".imggmail", {
-  delay: 800,
-  rotate: {
-    x: 80,
-    Y: 0,
-    z: 0,
-  },
-});
-
-ScrollReveal().reveal(".imgmaps", {
-  delay: 800,
-  rotate: {
-    x: 80,
-    Y: 0,
-    z: 0,
-  },
-});
-
-ScrollReveal().reveal(".imgwhats", {
-  delay: 800,
-  rotate: {
-    x: 80,
-    Y: 0,
-    z: 0,
-  },
-});
+ScrollReveal().reveal(
+  ".body-h1, .header-project, .tab-divider, .card, .container-text, .imggmail, .imgmaps, .imgwhats",
+  {
+    delay: 800,
+    rotate: {
+      x: 80,
+      Y: 0,
+      z: 0,
+    },
+  }
+);
